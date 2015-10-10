a7f07af5b77b4066180ea864be78689110eb2fd9
mrdemello <mrmello@gmail.com>
2015-10-10 15:29:27 -0300

    incluído um datagrid editável na tela de cadastro de tópicos (falta bastante coisa ainda)

--- projeto-webapp/src/main/java/br/edu/infnet/projeto/webapp/topico/TopicoMB.java
+++ projeto-webapp/src/main/java/br/edu/infnet/projeto/webapp/topico/TopicoMB.java
@@ -1,18 +1,29 @@
 package br.edu.infnet.projeto.webapp.topico;
 
+import java.util.List;
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
-
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import org.primefaces.event.RowEditEvent;
 import br.edu.infnet.projeto.ejb.topico.Topico;
 import br.edu.infnet.projeto.ejb.topico.TopicoBean;
 
-@RequestScoped 
-@ManagedBean 
+@ManagedBean
+@ViewScoped
 public class TopicoMB {
     @EJB
     private TopicoBean topicoBean;
-    private Topico topico = new Topico();
+    
+    private Topico topico;
+    private List<Topico> topicos;
+    
+    @PostConstruct
+    public void init() {
+    	atualizaView();
+    }
     
     public Topico getTopico() {
 		return topico;
@@ -21,9 +32,29 @@ public class TopicoMB {
 	public void setTopico(Topico topico) {
 		this.topico = topico;
 	}
+	
+    public List<Topico> getTopicos() {
+		return topicos;
+	}
 
-	public String adicionarTopico() {
+	public String adicionar() {
 		topicoBean.adicionar(topico);
+		atualizaView();
 		return "";
+    }
+	
+	private void atualizaView(){
+		topico = new Topico();
+		topicos = topicoBean.listar();
+	}
+	
+	public void onRowEdit(RowEditEvent event) {
+        FacesMessage msg = new FacesMessage("Car Edited", ((Topico) event.getObject()).getId().toString());
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+     
+    public void onRowCancel(RowEditEvent event) {
+        FacesMessage msg = new FacesMessage("Edit Cancelled", ((Topico) event.getObject()).getId().toString());
+        FacesContext.getCurrentInstance().addMessage(null, msg);
     }
 }
