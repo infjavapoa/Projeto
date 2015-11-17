459a6458cb092d3c5b4fc8859248760f978d5cd9
Márcio Roberto de Mello <mmarcio@gmail.com>
2015-11-17 17:46:43 -0200

    - Criada classe Timer para disparar ações de abrir avaliação e processar avaliação - Adicionado EJB de avaliação (chamado pela classe Timer) - Adicionado arquivo glassfish-resources para configuração do pool e jndi ficar na aplicação, e não mais no glassfish e alterado o persistence.xml para ficar de acordo

---
+++ projeto-ejb/src/main/java/br/edu/infnet/projeto/ejb/core/Timer.java
@@ -0,0 +1,24 @@
+package br.edu.infnet.projeto.ejb.core;
+
+import javax.ejb.EJB;
+import javax.ejb.Schedule;
+import javax.ejb.Stateless;
+import br.edu.infnet.projeto.ejb.avaliacao.AvaliacaoEJB;
+
+@Stateless
+public class Timer {
+	@EJB
+	AvaliacaoEJB avaliacaoEJB;
+	
+	//A cada 10 segundos
+	@Schedule(second="*/10", minute="*",hour="*", persistent=false)
+	public void abrirAvaliacoes(){
+	    avaliacaoEJB.abrirAvaliacoes();
+	}
+	
+	//A cada 10 segundos
+	@Schedule(second="*/10", minute="*",hour="*", persistent=false)
+	public void processarAvaliacoes(){
+		avaliacaoEJB.processarAvaliacoes();
+	}
+}
