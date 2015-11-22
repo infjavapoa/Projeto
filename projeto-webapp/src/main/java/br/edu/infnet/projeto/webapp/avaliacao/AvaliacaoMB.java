1d07b43cefcab356cc5b1d75fd064c865634ec44
Leandro <Leandro@Ultra-Licker>
2015-11-22 19:30:25 -0200

    Não quero mais ter que fazer merge!!!

--- projeto-webapp/src/main/java/br/edu/infnet/projeto/webapp/avaliacao/AvaliacaoMB.java
+++ projeto-webapp/src/main/java/br/edu/infnet/projeto/webapp/avaliacao/AvaliacaoMB.java
@@ -4,16 +4,12 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
-import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
-import javax.faces.context.FacesContext;
-import javax.swing.event.ChangeEvent;
 
 import org.primefaces.event.SelectEvent;
 
 import br.edu.infnet.projeto.ejb.avaliacao.Avaliacao;
-import br.edu.infnet.projeto.ejb.core.BaseEntity;
 import br.edu.infnet.projeto.ejb.core.Repositorio;
 import br.edu.infnet.projeto.ejb.infnet.Turma;
 import br.edu.infnet.projeto.ejb.questionario.Questionario;
@@ -25,107 +21,140 @@ public class AvaliacaoMB {
 	@EJB
     private Repositorio repositorio;
 	
-	private Avaliacao aval; 
+	private Avaliacao aval;
+	private List<Avaliacao> listaAvals;
 	
 	//turma
 	private Turma turma;
 	private Turma selTurma;
     private List<Turma> selTurmas;
-
-	//questionario
-    private Questionario questionario;
-    private List<Questionario> listaQuestionarios;
+    private List<Turma> filtroTurmas;
+    
+    //questionario
+  	private Questionario questionario;
+  	private Questionario selQuestionario;
+    private List<Questionario> selQuestionarios;
+    private List<Questionario> filtroQuestionarios;
 	
-    @PostConstruct
+	@PostConstruct
     public void init() {
     	atualizaView();
     }
-        
-    public Avaliacao getAval() {
+	
+	//Avaliacao
+	public Avaliacao getAval() {
 		return aval;
 	}
-
 	public void setAval(Avaliacao aval) {
 		this.aval = aval;
 	}
-
-	public Turma getTurma() {
+	public List<Avaliacao> getListaAvals() {
+		return listaAvals;
+	}
+	public void setListaAvals(List<Avaliacao> listaAvals) {
+		this.listaAvals = listaAvals;
+	}
+	
+	//INICIO TURMA
+    public Turma getTurma() {
 		return turma;
 	}
-
-	public void setTopico(Turma turma) {
+	public void setTurma(Turma turma) {
 		this.turma = turma;
-	}
-	
-	public List<Turma> getListaTurmas(){
-		
-		return repositorio.listar(Turma.class);
-		
-	}
-	
+	}	
+	public List<Turma> getListaTurmas(){		
+		return repositorio.listar(Turma.class);		
+	}	
 	public void setListaTurmas(List<Turma> listaTurmas) {
 	}
-	
 	public Turma getSelTurma() {
         return selTurma;
     }
- 
     public void setSelTurma(Turma selectedTurma) {
         this.selTurma = selectedTurma;
+        this.aval.setTurma(selectedTurma);
     }
- 
     public List<Turma> getSelectedTurmas() {
         return selTurmas;
     }
- 
-    
-    
+    public List<Turma> getFiltroTurmas() {
+        return filtroTurmas;
+    }
+    public void setFiltroTurmas(List<Turma> filtroTurmas) {
+        this.filtroTurmas = filtroTurmas;
+    }
     public void setSelectedTurmas(List<Turma> selectedTurmas) {
         this.selTurmas = selectedTurmas;
-        this.aval.setTurma(selTurma);
         atualizaView();
     }
-    
-    public void onChange(ChangeEvent event){
-    	
-    	FacesMessage msg = new FacesMessage("Turma informada" );
-    	FacesContext.getCurrentInstance().addMessage(null, msg);
-    	
-    	Turma t = new Turma();
-    	t.setId((long) 1);
-    	t.setNome("Teste de Turma");
-    	this.setSelTurma(t);
-    	//repositorio.obter(Turma, id);    	
-    }
-    
-    public void onRowSelect(SelectEvent event) {
-        FacesMessage msg = new FacesMessage("Turma selecionada", ((Turma) event.getObject()).getId().toString() );
-        FacesContext.getCurrentInstance().addMessage(null, msg);
-        
+    public void onRowTurSelect(SelectEvent event) {
+        //FacesMessage msg = new FacesMessage("Turma selecionada", ((Turma) event.getObject()).getId().toString() );
+        //FacesContext.getCurrentInstance().addMessage(null, msg);        
         //this.setListaTurmas(this.getListaTurmas());
     }
-	
-	public Questionario getQuestionario() {
+    //FINAL TURMA
+
+    
+  //INICIO QUESTIONARIO
+    public Questionario getQuestionario() {
 		return questionario;
 	}
-    
 	public void setQuestionario(Questionario questionario) {
 		this.questionario = questionario;
 	}
-	
-    public List<Questionario> getListaQuestionarios() {
-		return listaQuestionarios;
+	public List<Questionario> getListaQuestionarios(){		
+		return repositorio.listar(Questionario.class);		
+	}	
+	public void setListaQuestionarios(List<Questionario> listaQuestionarios) {
 	}
+	public Questionario getSelQuestionario() {
+        return selQuestionario;
+    }
+    public void setSelQuestionario(Questionario selectedQuestionario) {
+        this.selQuestionario = selectedQuestionario;
+        this.aval.setQuestionario(selectedQuestionario);
+    }
+    public List<Questionario> getSelectedQuestionario() {
+        return selQuestionarios;
+    }
+    public List<Questionario> getFiltroQuestionarios() {
+        return filtroQuestionarios;
+    }
+    public void setFiltroQuestionarios(List<Questionario> filtroQuestionarios) {
+        this.filtroQuestionarios = filtroQuestionarios;
+    }
+    public void setSelectedQuestionarios(List<Questionario> selectedQuestionarios) {
+        this.selQuestionarios = selectedQuestionarios;
+        atualizaView();
+    }	
+    public void onRowQuestSelect(SelectEvent event) {
+        //FacesMessage msg = new FacesMessage("Turma selecionada", ((Turma) event.getObject()).getId().toString() );
+        //FacesContext.getCurrentInstance().addMessage(null, msg);        
+        //this.setListaTurmas(this.getListaTurmas());
+    }
+    //FINAL TURMA
     
-    public void atualizaView(){
-		turma = new Turma();
-		questionario = new Questionario();
+	public void atualizaView(){
 		aval = new Avaliacao();
-		listaQuestionarios = repositorio.listar(Questionario.class);
-		this.setListaTurmas(repositorio.listar(Turma.class));
+		listaAvals = repositorio.listar(Avaliacao.class);
+		
+		//turma 
+		this.selTurmas = this.getListaTurmas();
+		this.filtroTurmas = this.selTurmas;
+		
+		//turma 
+		this.selQuestionarios = this.getListaQuestionarios();
+		this.filtroQuestionarios = this.selQuestionarios;
+		
 	}
-    
-    
-
+	
+	//Persistir
+	public void salvar() {
+		if (aval.getId() == null)
+			repositorio.adicionar(aval);
+		else
+			repositorio.atualizar(aval);
+		atualizaView();
+    }
 
 }
