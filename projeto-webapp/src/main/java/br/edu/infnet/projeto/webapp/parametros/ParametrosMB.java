8121eca68280d3e50515ec3edbbc9f81d0fd23ae
Marco <Marco@Marco>
2015-12-07 00:30:48 -0200

    Tela de parametros - preciso carregar os dados salvos no banco ainda não consegui fazer isso

---
+++ projeto-webapp/src/main/java/br/edu/infnet/projeto/webapp/parametros/ParametrosMB.java
@@ -0,0 +1,54 @@
+package br.edu.infnet.projeto.webapp.parametros;
+
+
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import br.edu.infnet.projeto.ejb.core.Repositorio;
+import br.edu.infnet.projeto.ejb.parametros.Parametros;
+
+
+
+@ManagedBean
+@ViewScoped
+public class ParametrosMB {
+	
+	@EJB
+    private Repositorio repositorio;
+	private Parametros parametros;
+	
+	@PostConstruct
+	public void init() {
+	   	atualizaView();
+	}
+	
+	public Parametros getParametros() {
+		return parametros;
+	}
+	public void setParametros(Parametros parametros) {
+		this.parametros = parametros;
+	}
+	
+	public void atualizaView(){
+		
+	}
+	
+	public void salvar() {
+		if (parametros.getId() == null)
+			repositorio.adicionar(parametros);
+		else
+			repositorio.atualizar(parametros);
+		atualizaView();
+		
+	
+	}
+	
+//	public void remover() {
+//		repositorio.remover(parametros);
+//		atualizaView();
+//    }
+	
+}
