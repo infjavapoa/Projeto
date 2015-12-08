783bac3ddc5e9ecd4be1361990cfe7c7cac1d275
Marco <Marco@192.168.56.1>
2015-12-08 13:26:19 -0200

    removido link "Template Email" do menu Tela parametros exibe parametros cadastrados, e salva atualização, ainda faltam detalhes na hora de salvar quando a tabela esta zerada, deu um erro mas vou  verificar isso a tarde. A opção arrastar  soltar as tags já esta sendo encaminhada.

--- projeto-webapp/src/main/java/br/edu/infnet/projeto/webapp/parametros/ParametrosMB.java
+++ projeto-webapp/src/main/java/br/edu/infnet/projeto/webapp/parametros/ParametrosMB.java
@@ -2,6 +2,8 @@ package br.edu.infnet.projeto.webapp.parametros;
 
 
 
+import java.util.List;
+
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
@@ -12,6 +14,8 @@ import br.edu.infnet.projeto.ejb.parametros.Parametros;
 
 
 
+
+
 @ManagedBean
 @ViewScoped
 public class ParametrosMB {
@@ -19,6 +23,9 @@ public class ParametrosMB {
 	@EJB
     private Repositorio repositorio;
 	private Parametros parametros;
+	
+	
+    private List<Parametros> listaParametros;
 	
 	@PostConstruct
 	public void init() {
@@ -34,9 +41,23 @@ public class ParametrosMB {
 	
 	public void atualizaView(){
 		
+		
+		if (parametros == null)
+		    parametros = new Parametros();
+					
+		listaParametros = repositorio.listar(Parametros.class);
+		if (listaParametros.size() == 1 )
+			   parametros = listaParametros.get(0);
+		
+		
+	}
+    
+    public List<Parametros> getListaParametros() {
+		return listaParametros;
 	}
 	
 	public void salvar() {
+		
 		if (parametros.getId() == null)
 			repositorio.adicionar(parametros);
 		else
