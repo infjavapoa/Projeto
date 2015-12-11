e949e642ac51d22421823a632ab2e94cd0087823
mrdemello <mrmello@gmail.com>
2015-12-10 23:56:12 -0200

    correção da chamada do método de excluir (já tinha alterado nas minhas)

--- projeto-webapp/src/main/java/br/edu/infnet/projeto/webapp/avaliacao/AvaliacaoMB.java
+++ projeto-webapp/src/main/java/br/edu/infnet/projeto/webapp/avaliacao/AvaliacaoMB.java
@@ -20,153 +20,179 @@ import br.edu.infnet.projeto.ejb.questionario.Questionario;
 @ManagedBean
 @ViewScoped
 public class AvaliacaoMB {
-	
+
 	@EJB
-    private Repositorio repositorio;
+	private Repositorio repositorio;
 	@EJB
 	ValidaAvaliacao validaAval;
-	
+
 	private Avaliacao aval;
 	private List<Avaliacao> listaAvals;
 	private List<Avaliacao> filtroAvals;
-	
-	//turma
+
+	// turma
 	private Turma turma;
 	private Turma selTurma;
-    private List<Turma> selTurmas;
-    private List<Turma> filtroTurmas;
-    
-    //questionario
-  	private Questionario questionario;
-  	private Questionario selQuestionario;
-    private List<Questionario> selQuestionarios;
-    private List<Questionario> filtroQuestionarios;
-	
+	private List<Turma> selTurmas;
+	private List<Turma> filtroTurmas;
+
+	// questionario
+	private Questionario questionario;
+	private Questionario selQuestionario;
+	private List<Questionario> selQuestionarios;
+	private List<Questionario> filtroQuestionarios;
+
 	@PostConstruct
-    public void init() {
-    	atualizaView();
-    }
-	
-	//Avaliacao
+	public void init() {
+		atualizaView();
+	}
+
+	// Avaliacao
 	public Avaliacao getAval() {
 		return aval;
 	}
+
 	public void setAval(Avaliacao aval) {
 		this.aval = aval;
 	}
+
 	public List<Avaliacao> getListaAvals() {
 		return listaAvals;
 	}
+
 	public void setListaAvals(List<Avaliacao> listaAvals) {
 		this.listaAvals = listaAvals;
 	}
-	
-	//INICIO TURMA
-    public Turma getTurma() {
+
+	// INICIO TURMA
+	public Turma getTurma() {
 		return turma;
 	}
+
 	public void setTurma(Turma turma) {
 		this.turma = turma;
-	}	
-	public List<Turma> getListaTurmas(){		
-		return repositorio.listar(Turma.class);		
-	}	
+	}
+
+	public List<Turma> getListaTurmas() {
+		return repositorio.listar(Turma.class);
+	}
+
 	public void setListaTurmas(List<Turma> listaTurmas) {
 	}
+
 	public Turma getSelTurma() {
-        return selTurma;
-    }
-    public void setSelTurma(Turma selectedTurma) {
-    	if (selectedTurma != null) { 
-	    	System.out.println("SEL TURMA" + aval);
+		return selTurma;
+	}
+
+	public void setSelTurma(Turma selectedTurma) {
+		if (selectedTurma != null) {
+			System.out.println("SEL TURMA" + aval);
 			System.out.println("Turma:" + aval.getTurma());
 			System.out.println("TurmaSel:" + this.getSelTurma());
-	        this.selTurma = selectedTurma;
-	        this.aval.setTurma(selectedTurma);
-        }
-    }
-    public List<Turma> getSelectedTurmas() {
-        return selTurmas;
-    }
-    public List<Turma> getFiltroTurmas() {
-        return filtroTurmas;
-    }
-    public void setFiltroTurmas(List<Turma> filtroTurmas) {
-        this.filtroTurmas = filtroTurmas;
-    }
-    public void setSelectedTurmas(List<Turma> selectedTurmas) {
-        this.selTurmas = selectedTurmas;
-        atualizaView();
-    }
-    public void onRowTurSelect(SelectEvent event) {
-        //FacesMessage msg = new FacesMessage("Turma selecionada", ((Turma) event.getObject()).getId().toString() );
-        //FacesContext.getCurrentInstance().addMessage(null, msg);        
-        //this.setListaTurmas(this.getListaTurmas());
-    }
-    //FINAL TURMA
+			this.selTurma = selectedTurma;
+			this.aval.setTurma(selectedTurma);
+		}
+	}
 
-    
-  //INICIO QUESTIONARIO
-    public Questionario getQuestionario() {
+	public List<Turma> getSelectedTurmas() {
+		return selTurmas;
+	}
+
+	public List<Turma> getFiltroTurmas() {
+		return filtroTurmas;
+	}
+
+	public void setFiltroTurmas(List<Turma> filtroTurmas) {
+		this.filtroTurmas = filtroTurmas;
+	}
+
+	public void setSelectedTurmas(List<Turma> selectedTurmas) {
+		this.selTurmas = selectedTurmas;
+		atualizaView();
+	}
+
+	public void onRowTurSelect(SelectEvent event) {
+		// FacesMessage msg = new FacesMessage("Turma selecionada", ((Turma)
+		// event.getObject()).getId().toString() );
+		// FacesContext.getCurrentInstance().addMessage(null, msg);
+		// this.setListaTurmas(this.getListaTurmas());
+	}
+
+	// FINAL TURMA
+
+	// INICIO QUESTIONARIO
+	public Questionario getQuestionario() {
 		return questionario;
 	}
+
 	public void setQuestionario(Questionario questionario) {
 		this.questionario = questionario;
 	}
-	public List<Questionario> getListaQuestionarios(){		
-		return repositorio.listar(Questionario.class);		
-	}	
+
+	public List<Questionario> getListaQuestionarios() {
+		return repositorio.listar(Questionario.class);
+	}
+
 	public void setListaQuestionarios(List<Questionario> listaQuestionarios) {
 	}
+
 	public Questionario getSelQuestionario() {
-        return selQuestionario;
-    }
-    public void setSelQuestionario(Questionario selectedQuestionario) {
-    	if (selectedQuestionario != null){
-	    	this.selQuestionario = selectedQuestionario;
-	        this.aval.setQuestionario(selectedQuestionario);
-    	}
-    }
-    public List<Questionario> getSelectedQuestionario() {
-        return selQuestionarios;
-    }
-    public List<Questionario> getFiltroQuestionarios() {
-        return filtroQuestionarios;
-    }
-    public void setFiltroQuestionarios(List<Questionario> filtroQuestionarios) {
-        this.filtroQuestionarios = filtroQuestionarios;
-    }
-    public void setSelectedQuestionarios(List<Questionario> selectedQuestionarios) {
-        this.selQuestionarios = selectedQuestionarios;
-        atualizaView();
-    }	
-    public void onRowQuestSelect(SelectEvent event) {
-        //FacesMessage msg = new FacesMessage("Turma selecionada", ((Turma) event.getObject()).getId().toString() );
-        //FacesContext.getCurrentInstance().addMessage(null, msg);        
-        //this.setListaTurmas(this.getListaTurmas());
-    }
-    //FINAL QUESTIONARIO
-       
-    
-	public void atualizaView(){
+		return selQuestionario;
+	}
+
+	public void setSelQuestionario(Questionario selectedQuestionario) {
+		if (selectedQuestionario != null) {
+			this.selQuestionario = selectedQuestionario;
+			this.aval.setQuestionario(selectedQuestionario);
+		}
+	}
+
+	public List<Questionario> getSelectedQuestionario() {
+		return selQuestionarios;
+	}
+
+	public List<Questionario> getFiltroQuestionarios() {
+		return filtroQuestionarios;
+	}
+
+	public void setFiltroQuestionarios(List<Questionario> filtroQuestionarios) {
+		this.filtroQuestionarios = filtroQuestionarios;
+	}
+
+	public void setSelectedQuestionarios(
+			List<Questionario> selectedQuestionarios) {
+		this.selQuestionarios = selectedQuestionarios;
+		atualizaView();
+	}
+
+	public void onRowQuestSelect(SelectEvent event) {
+		// FacesMessage msg = new FacesMessage("Turma selecionada", ((Turma)
+		// event.getObject()).getId().toString() );
+		// FacesContext.getCurrentInstance().addMessage(null, msg);
+		// this.setListaTurmas(this.getListaTurmas());
+	}
+
+	// FINAL QUESTIONARIO
+
+	public void atualizaView() {
 		aval = new Avaliacao();
 		listaAvals = repositorio.listar(Avaliacao.class);
-		
-		//turma 
+
+		// turma
 		this.selTurmas = this.getListaTurmas();
 		this.filtroTurmas = this.selTurmas;
-		
-		//questionario 
+
+		// questionario
 		this.selQuestionarios = this.getListaQuestionarios();
 		this.filtroQuestionarios = this.selQuestionarios;
-		
-		this.filtroAvals = this.getListaAvals(); 
-		this.selTurma = (null);  					//o método não vai gravar se for null
-		this.selQuestionario = (null);				//o método não vai gravar se for null
+
+		this.filtroAvals = this.getListaAvals();
+		this.selTurma = (null); // o método não vai gravar se for null
+		this.selQuestionario = (null); // o método não vai gravar se for null
 		this.aval.setTurma(null);
 		this.aval.setQuestionario(null);
 	}
-	
+
 	public List<Avaliacao> getFiltroAvals() {
 		return filtroAvals;
 	}
@@ -175,60 +201,61 @@ public class AvaliacaoMB {
 		this.filtroAvals = filtroAvals;
 	}
 
-	//Persistir
+	// Persistir
 	public void salvar() {
 		System.out.println("SALVAR" + aval);
 		System.out.println("Turma:" + aval.getTurma());
 		System.out.println("Quest:" + aval.getQuestionario());
 		System.out.println("TurmaSel:" + this.getSelTurma());
 		System.out.println("QuestSel:" + this.getSelQuestionario());
-				
+
 		try {
 			aval.setSituacao("F");
 			validaAval.setAval(aval);
-			System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX Antes XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");			
+			System.out
+					.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX Antes XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
 			if (aval.getId() == null) {
 				validaAval.valida(true);
-				repositorio.adicionar(aval);				
+				repositorio.adicionar(aval);
 			} else {
 				validaAval.valida(false);
-				repositorio.atualizar(aval);				
+				repositorio.atualizar(aval);
 			}
-			System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX Depois XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+			System.out
+					.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX Depois XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
 			atualizaView();
-		} catch (AvaliacaoInvalidaException exAval){
+		} catch (AvaliacaoInvalidaException exAval) {
 			System.out.println("Excdfdd: " + exAval.toString());
 			listaAvals = repositorio.listar(Avaliacao.class);
 			validaAval.validaExceptionAval(exAval);
 		} catch (Exception ex) {
 			validaAval.validaException(ex);
-		}	
-		
-    }
-	
-	
-	public void remover() throws AvaliacaoInvalidaException {
-		try{
+		}
+
+	}
+
+	public void remover(Avaliacao aval) {
+		try {
 			validaAval.setAval(aval);
 			validaAval.valida();
 			repositorio.remover(aval);
 			atualizaView();
-		} catch (AvaliacaoInvalidaException exAval){
-			System.out.println("Excdfdd: " + exAval.toString()); 
+		} catch (AvaliacaoInvalidaException exAval) {
+			System.out.println("Excdfdd: " + exAval.toString());
 			validaAval.validaExceptionAval(exAval);
-		} catch (PersistenceException perex){			
-			throw new AvaliacaoInvalidaException("Não é possivel excluir uma avaliação que já possui respostas!");			
+		} catch (PersistenceException perex) {
+			validaAval.validaException(perex);
 		} catch (Exception ex) {
 			validaAval.validaException(ex);
-		}		
-		
-    }
-	
+		}
+
+	}
+
 	public void editar() {
 		System.out.println("Turma:" + aval.getTurma());
 		System.out.println("Quest:" + aval.getQuestionario());
 		this.setSelTurma(aval.getTurma());
 		this.setSelQuestionario(aval.getQuestionario());
-    }
+	}
 
 }
